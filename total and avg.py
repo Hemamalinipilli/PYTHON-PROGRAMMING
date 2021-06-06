b2bc0c0540eb6523b3229861bf25afbf443868ae
Hemamalinipilli <84316344+Hemamalinipilli@users.noreply.github.com>
2021-06-06 15:14:09 +0530

    Add files via upload

---
+++ total and avg.py
@@ -0,0 +1,7 @@
+a=float(input("enter a value: "))
+b=float(input("enter b value: "))
+c=float(input("enter c value: "))
+sum=(a+b+c)
+avg=(a+b+c)/3
+print(f'total value=',sum)
+print(f'average value=',avg)
